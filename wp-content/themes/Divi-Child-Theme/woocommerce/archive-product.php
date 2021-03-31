de6c09add779169ffbe44e420a49b873b20faf6c
Mouy-im <75069309+Mouy-im@users.noreply.github.com>
2021-03-31 09:03:14 +0200

    new_widget

--- wp-content/themes/Divi-Child-Theme/woocommerce/archive-product.php
+++ wp-content/themes/Divi-Child-Theme/woocommerce/archive-product.php
@@ -33,6 +33,7 @@ do_action( 'woocommerce_before_main_content' );
 	<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
 		<h1 class="woocommerce-products-header__title page-title"><?php woocommerce_page_title(); ?></h1>
 	<?php endif; ?>
+	
 	<?php
 	/**
 	 * Hook: woocommerce_archive_description.
@@ -43,6 +44,9 @@ do_action( 'woocommerce_before_main_content' );
 	do_action( 'woocommerce_archive_description' );
 	?>
 </header>
+<div id="search_form">
+	<?php dynamic_sidebar( 'id_search_form' ); ?>
+</div>
 <?php
 if ( woocommerce_product_loop() ) {
 
